af276c1f8dbdf90b0d6a79db16b4cfba7c9b96b3
Miguel_Martinez <mm6980183@gmail.com>
2026-05-20 17:02:15 -0500

    Actualización Código

--- src/app.js
+++ src/app.js
@@ -1,7 +1,15 @@
 import express from 'express'
+import cors from 'cors'
 import userRoutes from './routes/user.routes.js'
 
 const app = express();
+
+// Habilitar CORS para el frontend en http://localhost:5173
+app.use(cors({
+	origin: 'http://localhost:5173',
+	methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+	allowedHeaders: ['Content-Type', 'Authorization']
+}));
 
 app.use(express.json());
 app.use(userRoutes);
